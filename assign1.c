745eedfb882b356e4167132d7942e3df12706305
janaat22 <janaat22@gmail.com>
2020-10-03 01:01:29 -0500

    newpath and pipe

--- assign1.c
+++ assign1.c
@@ -13,6 +13,9 @@ int main(void)
     char* prodv[500];
     char* consv[500];
     int i, j, k = 0;
+    char user_input[500];
+    char user_input_copy[500];
+    int terminate_while = 0;
     int ExitShell()
     {
         exit(0);
@@ -57,7 +60,7 @@ int main(void)
                         while(input != NULL)
                         {
                             consv[k] = input;
-                            printf("\n adding this to consv: %s", consv[k]);
+                            // printf("\n adding this to consv: %s", consv[k]);
                             input = strtok(NULL, " \t");
                             k++;
                         }
@@ -65,7 +68,7 @@ int main(void)
                         break;
                     }
                     prodv[j] = input;
-                    printf("\n adding this to prodv: %s", input);
+                    // printf("\n adding this to prodv: %s", input);
                     input = strtok(NULL, " \t");
                     j++;
                 }
@@ -85,8 +88,18 @@ int main(void)
                 return 0;                
             }
 
+            else if(strcmp(input, "newpath") == 0)
+            {
+                argv[0] = input;
+                char *path_to_set = strtok(user_input_copy, "\"");
+                argv[1] = strtok(NULL, "\"");
+                return 1;
+            
+            }
+
             argv[i] = input;
-            printf("\ninput added to argv now: %s\n", input);
+
+            // printf("\ninput added to argv : %s\n", input);
 
             input = strtok(NULL, " \t");
             
@@ -106,49 +119,8 @@ int main(void)
         
     }
 
-    while(1)
+    int execute_pipe()
     {
-        printf("\nMyShell:");
-        char user_input[500];
-        gets(user_input);
-        // printf("User input is : %s", user_input);
-        // printf("PATH is : %s", PATH_VAR);
-
-        char *input_command = strtok(user_input, " \t");        //parsing arguments
-        int parsed_output = parseInput(input_command);
-        if(parsed_output == 0)
-        {
-            printf("\n continuing!");
-            continue;
-        }
-        else if(parsed_output == 1)
-        {
-            if(strcmp(argv[0], "exit") == 0)         //handling exit command
-                {
-                    ExitShell();
-                }
-            else if (strcmp(argv[0], "cd") == 0)
-            {
-                if(chdir(argv[1])!=0)  //changing current working directory using chdir
-                {
-                printf("\npath not found: %s\n",argv[1]);
-                }  
-            }
-
-            int pid;
-            if((pid = fork()) == 0)
-            {
-                execvp(argv[0], argv);
-                _exit(0);
-            }
-            while(pid != wait(0));
-        }
-
-        else
-        {
-            printf("\n prod :%s", prodv[0]);
-            printf("\n consv :%s", consv[1]);
-
             int pd[2];
             pipe(pd);
 
@@ -164,24 +136,76 @@ int main(void)
                 execvp(consv[0], consv); // run command AFTER pipe character in userinput
                 // char *error = strerror(errno);
                 printf("unknown command\n");
-                return 0;
+                // terminate_while = 1;
             } else { // parent process
                 close(pd[0]);
                 dup2(pd[1], 1);
                 execvp(prodv[0], prodv); // run command BEFORE pipe character in userinput
                 // char *error = strerror(errno);
                 printf("unknown command\n");
-                return 0;
+                // terminate_while = 1;
             }
 
+            return 0;
+    }
+
+    while(!terminate_while)
+    {
+        printf("\nMyShell:");        
+        gets(user_input);
+        
+        strcpy(user_input_copy, user_input);
+
+        char *input_command = strtok(user_input, " \t");        //parsing arguments
+        int parsed_output = parseInput(input_command);
+        if(parsed_output == 0)
+        {
+            continue;
+        }
+        else if(parsed_output == 1)
+        {            
+            if(strcmp(argv[0], "exit") == 0)         //handling exit command
+                {
+                    ExitShell();
+                }
+            else if (strcmp(argv[0], "cd") == 0)
+            {
+                if(chdir(argv[1])!=0)  //changing current working directory using chdir
+                {
+                printf("\npath not found: %s\n",argv[1]);
+                }  
+                continue;
+            }
+            else if(strcmp(argv[0], "newpath") == 0)
+            {
+                char pathname[3000] = "PATH=";
+                char *putenv_arg = strcat(pathname,argv[1]);
+                if(putenv(putenv_arg)!= 0)
+                {
+                    printf("ERROR: new path not set!");
+                }
+                printf("\nPath is : %s\n", getenv("PATH"));
+                continue;
+                
+            }
+
+            int pid;
+            if((pid = fork()) == 0)
+            {
+                execvp(argv[0], argv);
+                _exit(0);
+            }
+            while(pid != wait(0));
         }
 
-       
-        
-        // printf("argv 1 & 2 is :%s,  %s", argv[0], argv[1]);
+        else
+        {
 
+            int return_pipe = execute_pipe();
+            printf("return from pipe %d", return_pipe);
+            continue;
 
+        }
     }
 return 0;
 }
-
